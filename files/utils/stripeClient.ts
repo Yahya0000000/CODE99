65888a89f5bc499c149012fb35a8f17689c66721
Yahya0000000 <ykreasat0@gmail.com>
2025-06-25 00:00:18 +0300

    Update stripeClient.ts

--- files/utils/stripeClient.ts
+++ files/utils/stripeClient.ts
@@ -1,3 +1,3 @@
 import Stripe from "stripe";
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: "2023-08-16" });
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: "2022-11-15" });
 export default stripe;
